9be435714008dcd3790e1cf0f46edad914cb037c
littlebittern <univ.dog@gmail.com>
2021-09-11 17:02:01 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext("2d");
 const bird = {
     x: canvas.width / 2,
     y: canvas.height / 2,
-
+    curBodySprite: "./sprites/body.png",
     head : {
         width: 8,
         height: 8,
@@ -32,7 +32,7 @@ const birdDraw = () => {
     const imageScale = 4;
     //Body Draw
     const imgBody = new Image();
-    imgBody.src = bird.body.sprites[0];
+    imgBody.src = bird.curBodySprite;
     imgBody.onload = () => {
         ctx.drawImage(imgBody, bird.x, bird.y, bird.body.width * imageScale, bird.body.height * imageScale);
     }
@@ -49,3 +49,17 @@ const resetMode = () => {
         bird.mode[v] = false; //배열로 접근해야 해당 값의 주소로 접근하기 때문에 값이 바뀜
     })
 }
+
+const changeSpriteFlying = () => {
+    if (bird.mode.flying) {
+        if (bird.curBodySprite == bird.body.sprites[0]) { 
+            bird.curBodySprite = bird.body.sprites[1];
+        }
+        else {
+            bird.curBodySprite = bird.body.sprites[0];
+        }
+    }
+}
+
+setInterval(changeSpriteFlying, 60);
+setInterval(birdDraw, 30);
