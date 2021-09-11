e44eef9fc9aa200f3c86374b52729e85881ffd4f
littlebittern <univ.dog@gmail.com>
2021-09-11 20:12:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const canvasHeight = 500;
 
 const bird = {
     x: canvas.width / 2,
-    y: canvas.height - 20,
+    y: canvas.height - 30,
     velocity_x: 0,
     velocity_y: 0,
     friction: 0.9,
+    gravity: 0.9,
     curBodySprite: "./sprites/body.png",
     head : {
         width: 8,
@@ -30,39 +31,94 @@ const bird = {
         walk: false,
     }
 }
+const modeSize = Object.keys(bird.mode).length;
+let curModeNum = null;
 
 const platforms = [];
-platformNum = 4;
+platformNum = 5;
+
+const foods = [];
+const colorArray = [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+    "#999966",
+    "#809980",
+    "#E6FF80",
+    "#1AFF33",
+    "#999933",
+    "#FF3380",
+    "#CCCC00",
+    "#66E64D",
+    "#4D80CC",
+    "#FF4D4D",
+    "#99E6E6",
+    "#6666FF"
+];
+maxFoodNum = 15;
+curFoodNum = 0;
 
 const createPlatform = () => {
     // 맨 밑 플랫폼
     platforms.push({
         x: 0,
-        y: 0,
+        y: canvasHeight - 10,
         width: canvasWidth,
-        height: canvasHeight / 20,
+        height: canvasHeight / 50,
     });
     // 공중 플랫폼
     directions = [[1,1], [-1,1], [-1,-1], [1,-1]];
-    for (var i=0; i<platformNum; i++) {
+    for (var i=0; i<platformNum - 1; i++) {
         platforms.push(
             {
-                x: canvasWidth / 2 + canvasWidth / 4 * directions[i][0],
-                y: canvasHeight / 2 + canvasHeight / 4 * directions[i][1],
                 width: canvasWidth / 6,
-                height: canvasHeight / 10,
+                height: canvasHeight / 50,
+                x: canvasWidth / 2 + (canvasWidth / 4 * directions[i][0]) - canvasWidth / 12,
+                y: canvasHeight / 2 + (canvasHeight / 4 * directions[i][1]) - canvasWidth / 50,
+                
             }
         );
     }
 }
 
+const createFood = () => {
+    let randomIndex = Math.floor(colorArray.length * Math.random());
+    let platformIndex = Math.floor(Math.random() * platformNum);
+    if (curFoodNum < maxFoodNum) {
+        let platform = platforms[platformIndex];
+        foods.push({
+            x: platform.x + platform.width * Math.random(),
+            y: platform.y - 10,
+            width: 5,
+            height: 7,
+            colorIndex: randomIndex,
+        })
+        curFoodNum++;
+    }
+}
+
 const drawCanvas = () => {
-    ctx.fillStyle = "#F0F8FF";
+    ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
 
-const modeSize = Object.keys(bird.mode).length;
-let curModeNum = null;
+const drawPlatform = () => {
+    ctx.fillStyle = "#000000";
+    for (var i=0; i<platformNum; i++) {
+        ctx.fillRect(platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height);
+    }
+}
+
+const drawFood = () => {
+    for (var i=0; i<curFoodNum; i++) {
+        ctx.fillStyle = colorArray[foods[i].colorIndex];
+        ctx.fillRect(foods[i].x, foods[i].y, foods[i].width, foods[i].height);
+    }
+}
 
 const drawBird = () => {
     const imageScale = 1;
@@ -98,12 +154,15 @@ const changeSpriteFlying = () => {
 }
 
 const main = () => {
+    //drawCanvas();
+    drawPlatform();
+    drawFood();
     drawBird();
     requestAnimationFrame(main);
 }
 
 const changeMode = () => {
-    let min = 5, max = 15;
+    let min = 7, max = 15;
     let rand = Math.floor(Math.random() * (max - min + 1) + min); 
     resetMode();
     
@@ -128,11 +187,13 @@ const changeMode = () => {
         default: break;
     }
 
+    createFood();
     setTimeout(changeMode, rand * 1000);
   }
   
 
 window.onload = () => {
+    createPlatform();
     main();
     changeMode();
 }
